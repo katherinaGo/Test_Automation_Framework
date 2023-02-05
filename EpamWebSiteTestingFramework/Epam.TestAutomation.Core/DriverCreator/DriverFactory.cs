046a49fc0872b3d0444a2ac543c6106118ab5e82
kateH <hovinkate@gmail.com>
2023-02-05 21:27:12 +0200

    removed setting resolution for browser

--- EpamWebSiteTestingFramework/Epam.TestAutomation.Core/DriverCreator/DriverFactory.cs
+++ EpamWebSiteTestingFramework/Epam.TestAutomation.Core/DriverCreator/DriverFactory.cs
@@ -1,4 +1,3 @@
-using System.Drawing;
 using Epam.TestAutomation.Core.Enums;
 using Epam.TestAutomation.Core.Helper;
 using Epam.TestAutomation.Utilities.Logger;
@@ -54,7 +53,6 @@ public static class DriverFactory
         chromeOptions.AddArgument("--start-maximized");
         var service = ChromeDriverService.CreateDefaultService();
         var chromeDriver = new ChromeDriver(service, chromeOptions, TimeSpan.FromMinutes(2));
-        chromeDriver.Manage().Window.Size = new Size(1512, 982);
         return chromeDriver;
     }
 
